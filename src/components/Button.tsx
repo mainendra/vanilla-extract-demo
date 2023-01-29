ff744e03537ffb4f998e0a92210da6cd1aeb061f
Mainendra <1897340+mainendra@users.noreply.github.com>
2023-01-29 15:31:45 -0500

    Removed button wrapper div

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,9 +5,7 @@ import { ButtonProps, buttonStyle } from "./Button.css";
 type themeType = keyof typeof themes;
 
 export function Button({ theme, children, ...btnProps } : { theme?: themeType, children?: ReactNode } & ButtonProps) {
-  return <div className={theme && themes[theme]} style={{'display': 'flex'}}>
-    <button className={buttonStyle({ ...btnProps })}>{children}</button>
-  </div>
+  return <button className={`${theme && themes[theme]} ${buttonStyle({ ...btnProps })}`}>{children}</button>;
 }
 
 export function Icon({ name  } : { name: string }) {
